a494306949910d554d42fc83518052ec67be9784
jaxpaws <jacksonbare@gmail.com>
2026-07-27 15:53:54 -0400

    Move focus to page header after navigating to new page

--- src/app/enter-data/services/page.tsx
+++ src/app/enter-data/services/page.tsx
@@ -1,9 +1,16 @@
+import { Metadata } from 'next';
 import ServicesForm from './servicesForm';
+
+export const metadata: Metadata = {
+  title: 'Enter Data | WARD',
+  description: 'Enter reporting data for various KPB activities',
+  icons: ['./favicon.png']
+};
 
 export default function Page() {
   return(
       <main id="main-content" className="px-8">
-          <h1 className="text-2xl" tabIndex={0}>Enter Data: Services Data</h1>
+          <h1 id="main-content-header" className="text-2xl" tabIndex={-1}>Enter Data: Services Data</h1>
           <ServicesForm></ServicesForm>
       </main>
   );
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,12 @@
-import type { Metadata } from "next";
+'use client'
+
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from './components/header';
 import Footer from './components/footer';
 import Link from "next/link";
+import { useEffect, useState } from 'react';
+import { usePathname } from 'next/navigation';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -15,17 +18,22 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export const metadata: Metadata = {
-  title: "KPB Litter Data",
-  description: "View and edit litter data",
-  icons: ["/favicon.png"]
-};
-
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [isInitialLoad, setIsInitialLoad] = useState<boolean>(true);
+  const pathname: string = usePathname();
+
+  useEffect(() => {
+    if (!isInitialLoad) {
+      document.getElementById('main-content-header')?.focus();
+    } else {
+      setIsInitialLoad(false);
+    }
+  }, [pathname]);
+
   return (
     <html
       lang="en"
@@ -34,7 +42,7 @@ export default function RootLayout({
       <body className="flex flex-col gap-8 justify-between h-full">
         <div className="flex flex-col gap-2">
           <Link
-            href="#main-content"
+            href="#main-content-header"
             className="absolute top-[-3em] bg-white focus:top-[0px] p-1 rounded-xs">
             Skip to main content
           </Link>
