3ddc6101405e38616a7ccfd83a8ccf6613343435
jaxpaws <jacksonbare@gmail.com>
2026-08-27 12:32:47 -0400

    refactor(event sql): stub unimplemented update functions

--- src/app/lib/event.sql.tsx
+++ src/app/lib/event.sql.tsx
@@ -229,20 +229,8 @@ export async function insertTreePlantingEvent(event: TreePlantingEventEntity): P
 
 // TODO: Implement UPDATE logic
 export async function updateAdoptASpotEvent(event: AdoptASpotEventEntity): Promise<number> {
-    let conn = null;
-    try {
-        conn = await getConnection();
-        const [result]: any = await conn.execute(
-            'INSERT INTO adopt_a_spot_cleanups (assignment_id, date, volunteer_count, volunteer_hours, litter_lbs, recycling_lbs) ' +
-            'VALUES (?, ?, ?, ?, ?, ?)',
-            [ event.assignmentId, event.date, event.volunteerCount, event.volunteerHours, event.litterLbs, event.recyclingLbs ]
-        );
-        conn.release();
-        return result.insertId;
-    } catch (err) {
-        console.error(`Error: Unable to insert Adopt-a-Spot event: ${err}`);
-        return -1;
-    }
+    console.log('Updating Adopt-a-Spot Event...');
+    return -1;
 }
 
 // TODO: Implement UPDATE logic
@@ -253,67 +241,14 @@ export async function updateBagSwapEvent(event: BagSwapEventEntity): Promise<num
 
 // TODO: Implement UPDATE logic
 export async function updateCleanTeamEvent(event: CleanTeamEventEntity): Promise<any> {
-    let conn = null;
-    try {
-        conn = await getConnection();
-        //await conn.query('START TRANSACTION');
-        const [result]: any = await conn.execute(
-            'INSERT INTO clean_team_events (date, event_desc, trash_lbs, recycling_lbs) ' +
-            'VALUES (?, ?, ?, ?)',
-            [ event.date, event.eventDesc, event.trashLbs, event.recyclingLbs ]
-        );
-        //await conn.query('COMMIT');
-        conn.release();
-        return result.insertId;
-    } catch (err) {
-        console.error(`Error: Unable to insert Roadside Litter event: ${err}`);
-        // if (conn) {
-        //     conn.query('ROLLBACK');
-        // }
-    }
+    console.log('Updating Clean Team Event...');
+    return -1;
 }
 
 // TODO: Implement UPDATE logic
 export async function updateCountyCleanupEvent(event: CountyCleanupEventEntity, bulkyItems: BulkyItemEntity[]): Promise<any> {
-    let conn = null;
-    try {
-        conn = await getConnection();
-        await conn.query('START TRANSACTION');
-        const [result]: any = await conn.execute(
-            'INSERT INTO county_cleanups (date, tire_count, tire_lbs, ' +
-                'paint_can_and_household_chemical_count, paint_can_and_household_chemical_lbs, ' +
-                ' bulky_item_lbs) ' +
-            'VALUES (?, ?, ?, ?, ?, ?)',
-            [ 
-                event.date, event.tireCount, event.tireLbs,
-                event.paintCanAndHouseholdChemicalCount, event.paintCanAndHouseholdChemicalLbs,
-                event.bulkyItemsLbs
-            ]
-        );
-        if (result && result.insertId) {
-            let bulkyItemValuePlaceholders: string = '';
-            let bulkyItemValues: any[] = [];
-            bulkyItems.map((item) => {
-                bulkyItemValuePlaceholders += ' (?, ?, ?),';
-                bulkyItemValues.push(item.bulkyItemRefId);
-                bulkyItemValues.push(result.insertId); // TODO: Make this the event id
-                bulkyItemValues.push(item.quantity);
-            });
-            await conn.execute(
-                'INSERT INTO county_cleanups_bulky_items (bulky_item_ref_id, county_cleanup_id, quantity) ' +
-                `VALUES${bulkyItemValuePlaceholders.slice(0, -1)}`,
-                bulkyItemValues
-            );
-        }
-        await conn.query('COMMIT');
-        conn.release();
-        return result.insertId;
-    } catch (err) {
-        console.error(`Error: Unable to insert County Cleanup event: ${err}`);
-        if (conn) {
-            conn.query('ROLLBACK');
-        }
-    }
+    console.log('Updating County Cleanup Event...');
+    return -1;
 }
 
 // TODO: Implement UPDATE logic
@@ -324,59 +259,20 @@ export async function updateEducationEvent(event: EducationEventEntity): Promise
 
 // TODO: Implement UPDATE logic
 export async function updateGroupCleanupEvent(event: GroupCleanupEventEntity): Promise<number> {
-    let conn = null;
-    try {
-        conn = await getConnection();
-        const [result]: any = await conn.execute(
-            'INSERT INTO group_cleanups (organization_id, location_id, date, volunteer_count, volunteer_hours, litter_lbs, recycling_lbs) ' +
-            'VALUES (?, ?, ?, ?, ?, ?, ?)',
-            [ event.organizationId, event.locationId, event.date, event.volunteerCount, event.volunteerHours, event.litterLbs, event.recyclingLbs ]
-        );
-        conn.release();
-        return result.insertId;
-    } catch (err) {
-        console.error(`Error: Unable to insert Group Cleanup event: ${err}`);
-        return -1;
-    }
+    console.log('Updating Group Cleanup Event...');
+    return -1;
 }
 
 // TODO: Implement UPDATE logic
-export async function updateRoadsideLitterEvent(event: RoadsideLitterEventEntity, districts: DistrictEntity[], bulkyItems: BulkyItemEntity[]): Promise<void> {
-    let conn = null;
-    try {
-        conn = await getConnection();
-        await conn.execute('START TRANSACTION');
-        const [result] = await conn.execute(
-            'UPDATE roadside_litter_cleanups (date, litter_lbs, recycling_lbs, locations) ' +
-            'OUTPUT INSERTED.id' +
-            'VALUES (?, ?, ?, ?)',
-            [ event.date, event.litterLbs, event.recyclingLbs, event.locations ]
-        );
-        await conn.execute('COMMIT');
-        conn.release();
-    } catch (err) {
-        console.error(`Error while executing query to insert Roadside Litter event`);
-        if (conn) {
-            conn.execute('ROLLBACK');
-        }
-    }
+export async function updateRoadsideLitterEvent(event: RoadsideLitterEventEntity, districts: DistrictEntity[], bulkyItems: BulkyItemEntity[]): Promise<number> {
+    console.log('Updating Roadside Litter Event...');
+    return -1;
 }
 
 // TODO: Implement UPDATE logic
-export async function updateTrashRoutesEvent(event: TrashRoutesEventEntity): Promise<any> {
-    let conn = null;
-    try {
-        conn = await getConnection();
-        const [result]: any = await conn.execute(
-            'INSERT INTO trash_can_routes (date, trash_lbs, recycling_lbs) ' +
-            'VALUES (?, ?, ?)',
-            [ event.date, event.trashLbs, event.recyclingLbs ]
-        );
-        conn.release();
-        return result.insertId;
-    } catch (err) {
-        console.error(`Error: Unable to insert Trash Routes event: ${err}`);
-    }
+export async function updateTrashRoutesEvent(event: TrashRoutesEventEntity): Promise<number> {
+    console.log('Updating Trash Can Routes Event...');
+    return -1;
 }
 
 // TODO: Implement UPDATE logic
