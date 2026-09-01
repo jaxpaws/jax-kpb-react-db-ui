7df20f60ab876ee0600de14a5fdcc4739325c80a
jaxpaws <jacksonbare@gmail.com>
2026-09-01 10:54:04 -0400

    feat(textbox): add aria-label to error icon

--- src/app/components/textbox/textbox.model.tsx
+++ src/app/components/textbox/textbox.model.tsx
@@ -1,13 +1,13 @@
 export interface TextboxModel {
-    inputId: string,
-    inputType: string,
-    inputName?: string,
-    labelText: string,
-    descriptionText?: string,
-    maxlength?: number,
-    step?: number,
-    width?: string,
-    isRequired?: boolean,
-    labelFontWeight?: string,
-    errorText?: string
+    inputId: string;
+    inputType: string;
+    inputName?: string;
+    labelText: string;
+    descriptionText?: string;
+    maxlength?: number;
+    step?: number;
+    width?: string;
+    isRequired?: boolean;
+    labelFontWeight?: string;
+    errorText?: string;
 }
