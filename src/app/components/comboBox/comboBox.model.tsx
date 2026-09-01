a68b73777e90d00442de036d4bfd2ab34a367b45
jaxpaws <jacksonbare@gmail.com>
2026-09-01 10:46:14 -0400

    feat(comboBox): add prop to set current value

--- src/app/components/comboBox/comboBox.model.tsx
+++ src/app/components/comboBox/comboBox.model.tsx
@@ -3,6 +3,7 @@ export interface ComboBoxModel {
     searchInputId: string;
     listboxId: string;
     buttonId: string;
+    value?: string;
     listAriaLabel?: string;
     options: string;
     isRequired: boolean;
